32ec0bbcacadf0bc37e16c49957310838ae399b3
sweta <swetat007@protonmail.com>
2023-03-27 11:44:33 +0200

    Add routes for register & login page

--- project/routes.py
+++ project/routes.py
@@ -1,7 +1,7 @@
 from project import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from project.models import Item, User
-from project.forms import RegisterForm
+from project.forms import RegisterForm, LoginForm
 from project import db
 
 @app.route('/')
@@ -20,14 +20,19 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data, 
                               email_address=form.email_address.data, 
-                              password_hash=form.password1.data)
+                              password=form.password1.data)
+        
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
-    if forms.errors != {}:#if there are not errors after the validation
-        pass
+    if form.errors != {}:#if there are not errors after the validation
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+           
     return render_template("register.html", form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login_page():
-    pass
+    form = LoginForm()
+
+    return render_template("login.html", form=form)

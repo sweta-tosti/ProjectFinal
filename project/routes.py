da73c9a1609af37fe21b1acaf294e6c0898a50c0
sweta <swetat007@protonmail.com>
2023-04-12 11:42:08 +0200

    Changes in register_page & market_page

--- project/routes.py
+++ project/routes.py
@@ -1,18 +1,31 @@
-from project import app
-from flask import render_template, redirect, url_for, flash
+from project import app, db
+from flask import render_template, redirect, url_for, flash, request
 from project.models import Item, User
-from project.forms import RegisterForm, LoginForm
-from project import db
+from project.forms import RegisterForm, LoginForm, PurchaseItemForm
+from flask_login import login_user, logout_user, login_required, current_user
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template ("home.html")
 
-@app.route('/market')
+@app.route('/market', methods=["GET", "POST"])
+@login_required
 def market_page():
-    items = Item.query.all()
-    return render_template("market.html", items = items )
+    purchase_form = PurchaseItemForm()
+    if request.method == 'POST':
+        purchased_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            p_item_object.owner = current_user.id
+            current_user.budget -= p_item_object.price
+            db.session.commit()
+
+
+
+    items = Item.query.filter_by(owner=None)
+    
+    return render_template("market.html", items = items, purchase_form=purchase_form )
 
 @app.route('/register', methods=["GET", "POST"])
 def register_page():
@@ -21,10 +34,13 @@ def register_page():
         user_to_create = User(username=form.username.data, 
                               email_address=form.email_address.data, 
                               password=form.password1.data)
-        
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f'Account created succeesfully! yor are now logged in as {user_to_create.username} ', category='success')
+
         return redirect(url_for('market_page'))
+    
     if form.errors != {}:#if there are not errors after the validation
         for err_msg in form.errors.values():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
@@ -34,5 +50,22 @@ def register_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect (url_for('market_page'))
+        else:
+            flash('Username and password are not match! Please try again', category='danger')
 
     return render_template("login.html", form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout_page():
+    logout_user()
+    flash("you have been logged out!", category='info')
+    return redirect(url_for("home_page"))

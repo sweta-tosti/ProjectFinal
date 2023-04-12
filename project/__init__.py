b32b42e98b8f15c50d2cf53e38c712a8053fed10
sweta <swetat007@protonmail.com>
2023-04-12 11:56:23 +0200

    Add login manager

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config['SECRET_KEY'] = '634a0cf5cbd636bc98b1228b'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from project import routes
 

f3e6dac6710470a090f7f522def280e0acdfe09f
sweta <swetat007@protonmail.com>
2023-04-12 11:40:05 +0200

    Add login_manager, load_user & add function 'buy' in class Item

--- project/models.py
+++ project/models.py
@@ -1,7 +1,12 @@
-from project import db
+from project import db, login_manager
 from project import bcrypt
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=40), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
@@ -10,6 +15,12 @@ class User(db.Model):
     items = db.relationship('Item', backref="owned_user", lazy=True)
 
     @property
+    def prettier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f"{str(self.budget)[:-3]},{str(self.budget)[-3:]}$"
+        else:
+            return f"{self.budget}$"    
+    @property
     def password(self):
         return self.password
     
@@ -17,7 +28,11 @@ class User(db.Model):
     def password(self, plain_text_password):
         self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
 
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -30,3 +45,8 @@ class Item(db.Model):
     """Return a string representation of the object"""
     def __repr__(self):
         return f'Item {self.name}'  
+    
+    def buy(self, user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
